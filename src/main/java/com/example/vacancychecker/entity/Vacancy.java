d3e4279a69b042e7781916e75a929395f8743c43
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2023-05-05 12:11:13 +0300

    Refactoring the loading of vacancies

--- src/main/java/com/example/vacancychecker/entity/Vacancy.java
+++ src/main/java/com/example/vacancychecker/entity/Vacancy.java
@@ -1,33 +1,75 @@
 package com.example.vacancychecker.entity;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import java.util.List;
-import lombok.Data;
+import java.util.Objects;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class Vacancy {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String slug;
+    @Column(name = "company_name")
     private String companyName;
     private String title;
-    @Column(length = 10000)
+    @Column(columnDefinition = "TEXT")
     private String description;
     private boolean remote;
     private String url;
     @ElementCollection
+    @Column(name = "tag")
     private List<String> tags;
     @ElementCollection
+    @CollectionTable(name = "job_types")
+    @Column(name = "job_type")
     private List<String> jobTypes;
     private String location;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
     private Long createdAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vacancy)) return false;
+        Vacancy vacancy = (Vacancy) o;
+        return Objects.equals(getUrl(), vacancy.getUrl());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUrl());
+    }
+
+    @Override
+    public String toString() {
+        return "Vacancy{"
+                + "id=" + id
+                + ", slug='" + slug + '\''
+                + ", companyName='" + companyName + '\''
+                + ", title='" + title + '\''
+                + ", description='" + description + '\''
+                + ", remote=" + remote
+                + ", url='" + url + '\''
+                + ", tags=" + tags
+                + ", jobTypes=" + jobTypes
+                + ", location='" + location + '\''
+                + ", createdAt=" + createdAt
+                + '}';
+    }
 }
--- src/main/java/com/example/vacancychecker/service/VacancyLoader.java
+++ src/main/java/com/example/vacancychecker/service/VacancyLoader.java
@@ -6,41 +6,49 @@ import com.example.vacancychecker.entity.Vacancy;
 import com.example.vacancychecker.repository.VacancyRepository;
 import com.example.vacancychecker.service.dto.VacancyDto;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.http.HttpMethod;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 @AllArgsConstructor
 public class VacancyLoader {
-    public static final String URL = "https://www.arbeitnow.com/api/job-board-api";
+    public static final String URL = "https://www.arbeitnow.com/api/job-board-api?page={page}";
     private static final int HOUR = 60 * 60 * 1000;
     private final RestTemplate restTemplate = new RestTemplate();
     private final VacancyRepository repository;
 
     @Scheduled(fixedRate = HOUR)
+    @Cacheable("vacancies")
     public void loadJobs() {
-        for (int page = 1; page <= 5; page++) {
-            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(URL)
-                    .queryParam("page", page);
-            String url = builder.toUriString();
+        List<MyResponse> responses = IntStream.rangeClosed(1, 5)
+                .parallel()
+                .mapToObj(this::fetchVacancies)
+                .collect(Collectors.toList());
 
-            ResponseEntity<MyResponse> response = restTemplate.exchange(url, HttpMethod.GET,
-                    null, MyResponse.class);
-            VacancyDto[] responseData = response.getBody().getData();
+        List<Vacancy> vacancies = responses.stream()
+                .flatMap(response -> Arrays.stream(response.getData()))
+                .map(this::toVacancy)
+                .filter(vacancy -> !repository.existsVacancyByUrl(vacancy.getUrl()))
+                .collect(Collectors.toList());
 
-            if (responseData != null) {
-                Arrays.stream(responseData)
-                        .map(this::toVacancy)
-                        .filter(vacancy -> !repository.existsVacancyByUrl(vacancy.getUrl()))
-                        .forEach(repository::save);
-            }
+        if (!vacancies.isEmpty()) {
+            repository.saveAll(vacancies);
         }
+    }
+
+    @Cacheable(value = "vacancies", key = "{#page}")
+    public MyResponse fetchVacancies(int page) {
+        String url = URL.replace("{page}", String.valueOf(page));
+        ResponseEntity<MyResponse> response = restTemplate.getForEntity(url, MyResponse.class);
+        return response.getBody();
     }
 
     @Data
